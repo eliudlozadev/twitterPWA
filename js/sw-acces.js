2caff80063b62ca5f2313eeba7281d33e9153fcc
Eliud Hernandez Loza <eliuddev@hotmail.com>
2022-04-07 22:42:19 -0500

    Nuevos cambios

--- js/sw-acces.js
+++ js/sw-acces.js
@@ -1,10 +1,21 @@
-function actualizaCacheDinamico(dynamicCache, req, res) {
-    if (res.ok) {
-        return caches.open(dynamicCache).then(cache => {
-            cache.put(req, res.clone());
+// Guardar  en el cache dinamico
+function actualizaCacheDinamico( dynamicCache, req, res ) {
+
+
+    if ( res.ok ) {
+
+        return caches.open( dynamicCache ).then( cache => {
+
+            cache.put( req, res.clone() );
+            
             return res.clone();
+
         });
+
     } else {
         return res;
     }
+
+
+
 }
--- sw.js
+++ sw.js
@@ -1,12 +1,14 @@
 // imports
 importScripts('js/sw-acces.js');
 
-const STATIC_CACHE = "static-v1";
-const DYNAMIC_CACHE = "dynamic-v1";
-const INMUTABLE_CACHE = "inmutable-v1";
+
+const STATIC_CACHE    = 'static-v4';
+const DYNAMIC_CACHE   = 'dynamic-v2';
+const INMUTABLE_CACHE = 'inmutable-v1';
+
 
 const APP_SHELL = [
-    "/",
+    // '/',
     "index.html",
     "css/style.css",
     "images/favicon.ico",
@@ -16,6 +18,7 @@ const APP_SHELL = [
     "images/avs/porsche_2.jpg",
     "images/avs/sukhoi_35.jpg",
     "images/avs/sukhoi_35_2.jpg"
+
 ];
 
 const APP_SHELL_INMUTABLE = [
@@ -27,28 +30,34 @@ const APP_SHELL_INMUTABLE = [
 ];
 
 
-self.addEventListener("install", event => {
 
-    const cacheStatic = caches.open(STATIC_CACHE).then(cache => {
-        cache.addAll(APP_SHELL);
-    });
-    
-    const cacheInmutable = caches.open(INMUTABLE_CACHE).then(cache => {
-        cache.addAll(APP_SHELL_INMUTABLE);
-    });
+self.addEventListener('install', e => {
 
-    event.waitUntil(Promise.all([cacheStatic,cacheInmutable]));
+
+    const cacheStatic = caches.open( STATIC_CACHE ).then(cache => 
+        cache.addAll( APP_SHELL ));
+
+    const cacheInmutable = caches.open( INMUTABLE_CACHE ).then(cache => 
+        cache.addAll( APP_SHELL_INMUTABLE ));
+
+
+
+    e.waitUntil( Promise.all([ cacheStatic, cacheInmutable ])  );
+
 });
 
 
-self.addEventListener("activate", event => {
-    
-    const respuesta = caches.keys().then(keys => {
-        
-        keys.forEach(key => {
+self.addEventListener('activate', e => {
 
-            //static cache
-            if (key !== STATIC_CACHE && key.includes('static')) {
+    const respuesta = caches.keys().then( keys => {
+
+        keys.forEach( key => {
+
+            if (  key !== STATIC_CACHE && key.includes('static') ) {
+                return caches.delete(key);
+            }
+
+            if (  key !== DYNAMIC_CACHE && key.includes('dynamic') ) {
                 return caches.delete(key);
             }
 
@@ -56,21 +65,36 @@ self.addEventListener("activate", event => {
 
     });
 
-    event.waitUntil(respuesta);
+    e.waitUntil( respuesta );
+
 });
 
 
-self.addEventListener('fetch', event => {
-    
-    const respuesta = caches.match(event.request).then(respuesta => {
-            if (respuesta) {
-                return respuesta;
-            } else {
-                return fetch(event.request).then( newRes => {
-                    return actualizaCacheDinamico(DYNAMIC_CACHE, event.request, newRes);
-                });
-            };
+
+
+self.addEventListener( 'fetch', e => {
+
+
+    const respuesta = caches.match( e.request ).then( res => {
+
+        if ( res ) {
+            return res;
+        } else {
+
+            return fetch( e.request ).then( newRes => {
+
+                return actualizaCacheDinamico( DYNAMIC_CACHE, e.request, newRes );
+
+            });
+
         }
-    );
-    event.respondWith(respuesta);
+
+    });
+
+
+
+    e.respondWith( respuesta );
+
 });
+
+
